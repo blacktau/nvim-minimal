76b9a09c64908a2e0231a42f2fc95aa711b70adf
Sean Garrett <1028192+blacktau@users.noreply.github.com>
2025-05-05 12:11:46 +0100

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,8 @@
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.undofile = true
+vim.g.mapleader = " "
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
